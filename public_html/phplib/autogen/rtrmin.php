a49131eb386a4d186894655eb214b5591e317f4b
David Beveridge <dave@bevhost.com>
2012-09-19 13:05:13 +1000

    Update public_html/phplib/autogen/rtrmin.php

--- public_html/phplib/autogen/rtrmin.php
+++ public_html/phplib/autogen/rtrmin.php
@@ -1,5 +1,5 @@
 <?php include('phplib/prepend.php');
-page_open(array("sess" => "rtrmin_Session")); ?>
+page_open(array("sess" => "fetid_Session")); ?>
 <html><head><title>smeg rtrmin Database</title></head><body>
 <font class=bigTextBold align="CENTER">rtrmin Database</font>
 <font class='bigTextBold' align='CENTER'><a href="<?php $sess->purl("EventLog.php") ?>">Event Log</a></font>
